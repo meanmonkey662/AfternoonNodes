8b8896c6df5f4f7e1dcd39dc3e0d3399454e88de
meanmonkey662 <coolkoch8535@gmail.com>
2016-03-29 13:16:24 -0600

    timer

--- Controller/NodeController.cpp
+++ Controller/NodeController.cpp
@@ -7,6 +7,7 @@
 
 #include "NodeController.h"
 #include <iostream>
+#include <stdlib.h
 
 using namespace std;
 
@@ -35,4 +36,40 @@ void NodeController :: start()
 	arrayTimer.stopTimer();
 	arrayTimer.dislayTimerInformation();
 
+}
+
+void NodeController::sortData();
+{
+    /*
+     creare a CTECList, CTECArray
+     Fill them with random stuff(numbers)
+     Start a timer, sort, stop timer, display info for list
+     Start a timer, sort, stop timer, display info for array
+     */
+    CTECArray<int> randomNumberArray(5000);
+    CTECList<int> randomNumberList;
+    intmyCPlusPlusArray[5000];
+    
+    for(int spot = 0; spot <5000; spot++)
+    {
+        int myRandom = rand();
+        randomNumberArray.set(spot, myRandom);
+        randomNumberList,addToEnd(myRandom);
+        myCPlusPlusArray[spot] = myRandom;
+    }
+    
+    Timer sortTimer;
+    sortTimer.startTimer();
+    randomNumberArray.selectionSort();
+    sortTimer.stopTimer();
+    sortTimer.displayTimerInformation();
+    
+    sortTimer.resetTimer();
+    
+    sortTimer.startTimer();
+    std::sort(std::begin(mCPlusPlusArray), std::end(myCPlusPlusArray));
+    sortTimer.stopTimer();
+    sortTimer.displayTimerInformation();
+    
+    sortTimer.resetTimer();
 }
