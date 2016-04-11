237a25330589f529035aa419457ed959637b1a08
meanmonkey662 <coolkoch8535@gmail.com>
2016-04-11 11:59:26 -0600

    tree node

---
+++ Model/TreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TreeNode.cpp
+//  AfternoonNodes
+//
+//  Created by Koch, Kyle on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "TreeNode.hpp"
---
+++ Model/TreeNode.hpp
@@ -0,0 +1,14 @@
+//
+//  TreeNode.hpp
+//  AfternoonNodes
+//
+//  Created by Koch, Kyle on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include "Node.h"
+
+#endif /* TreeNode_hpp */
