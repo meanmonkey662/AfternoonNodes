528b06292f21d0f30360595bc14badd533fb5e56
meanmonkey662 <coolkoch8535@gmail.com>
2016-04-25 12:09:13 -0600

    added CTECGraph

---
+++ Model/CTECGraph.cpp
@@ -0,0 +1,10 @@
+//
+//  CTECGraph.cpp
+//  AfternoonNodes
+//
+//  Created by Koch, Kyle on 4/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "CTECGraph.hpp"
+
---
+++ Model/CTECGraph.hpp
@@ -0,0 +1,40 @@
+//
+//  CTECGraph.hpp
+//  AfternoonNodes
+//
+//  Created by Koch, Kyle on 4/25/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef CTECGraph_hpp
+#define CTECGraph_hpp
+#include <set>
+#include <stdio.h>
+
+namespace CTECData
+{
+    template <class Type>
+    class CTECGraph
+    {
+    private:
+        static const int MAXIMUM = 20;
+        bool adjacencyMatrix [MAXIMUM] [MAXIMUM];
+        Type labels[MAXIMUM];
+        int manyVertices;
+    public:
+        CTECGraph();
+        ~CTECGraph();
+        
+        
+        void addVertex(const Type& value);
+        void addEdge(int source, int target);
+        void removeEdge(int source, int target);
+        Type& operator [] (int vertex);
+        Type operator [] (int vertex) const;
+        int size();
+        bool isEdge(int source, int target)const;
+        std::set<int> neighbors(int vertex) const;
+    };
+}
+
+#endif /* CTECGraph_hpp */
